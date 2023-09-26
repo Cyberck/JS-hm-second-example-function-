a98cf73e17f718900dbc4a9ed37bd292e5fcddb7
unknown <Baxtiyorddushaev@gmail.com>
2023-09-27 00:47:09 +0500

    first

---
+++ script-2.js
@@ -0,0 +1,18 @@
+
+
+// Funcsii - eto chast koda katoraya mojet bit vizvana pavtorno v raznih mestah vasheva koda 
+// Ovyavleniye koda proishodit pri pomoshi kluchevogo slovo function
+// primer sozdaniye funksii ' function imya funcsii () {} '
+
+
+ function sum(num1, num2) {
+   let result = num1 + num2 * 3;
+   return result;// tut nado govarit vozvrashay resultat
+ }
+ console.log(sum(10, 30));//eto argumenti
+
+ 
+
+
+
+
